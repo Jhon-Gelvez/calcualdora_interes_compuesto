800e41bcdc23204fb01d22c6d5a355583d27ef82
David27Ju <jbernaltat@gmail.com>
2025-11-05 09:26:21 -0500

    git inint

---
+++ Jhon_Gelvez.py
@@ -0,0 +1,14 @@
+#calculadora interes compuesto 
+
+# entrada de datos por el usuario 
+# aporte_inicial 
+# aporte_adicional #valor mensual o anual 
+# tasa de interes - anual o mensual
+# numero de periodos 
+
+# imprimir el total en aportes 
+# el total de interes se calcula cada mes y se suma para obtener este dato
+# total
+# una grafica 
+# opcional una tabla
+
